b70ace7c9733baafd1a7a5fda9d852d0cddc6bdd
ge shu <92536162+Eronshu@users.noreply.github.com>
2022-12-16 10:56:51 -0500

    new branch

--- src/AdminReport.js
+++ src/AdminReport.js
@@ -1,27 +1,84 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {getOrders} from "./api/adminReportApi";
+import {getVisitEvents} from "./api/adminReportApi"
+import {Button, message, Table} from "antd";
+
 
 export function AdminReport() {
     const [data, setData] = useState([]);
-    const [error, setError] = useState(null);
     const [month, setMonth] = useState('');
     const [year, setYear] = useState('');
+    const [data2, setData2] = useState([]);
 
-    function getOrderReport(){
-        getOrders({month,year}).then(res=>{
-                console.log(data);
-                debugger
-                setData(res.data)
-            }
-        ).catch(err => [
-            setError(err)
-        ])
-        if (error) {
-            return <p>An error occurred: {error.message}</p>;
-        }
+    const [fromMonth, setFromMonth] = useState('');
+    const [fromYear, setFromYear] = useState('');
+    const [toMonth, setToMonth] = useState('');
+    const [toYear, setToYear] = useState('');
+    const [type, setType] = useState('');
+
+    function getOrderReport() {
+        getOrders(month, year)
+            .then(res => {
+                    setData(res.data.data)
+                }
+            ).catch(err => {
+            message.error(err.response.msg)
+        })
     }
+
+    function getUsageVisitEvents() {
+        getVisitEvents(fromMonth, fromYear, toMonth, toYear, type)
+            .then((res) => {
+                setData2(res.data.data);
+            })
+            .catch((err) => {
+                message.error(err.response.msg)
+            });
+    }
+
+    const columnsO = [
+        {
+            title: 'item_id',
+            dataIndex: 'item_id',
+        },
+        {
+            title: 'item_name',
+            dataIndex: 'item_name',
+        },
+        {
+            title: 'quantity',
+            dataIndex: 'quantity',
+        },
+        {
+            title: 'price',
+            dataIndex: 'price',
+        },
+    ];
+    const columnsV = [
+        {
+            title: 'Event Type',
+            dataIndex: 'event_type',
+        },
+        {
+            title: 'IP Address',
+            dataIndex: 'ip_address',
+        },
+        {
+            title: 'timestamp',
+            dataIndex: 'timestamp',
+        },
+        {
+            title: 'Data',
+            dataIndex: 'data',
+        },
+    ];
+    let VIEW_ITEM = "View Item";
+    let CART_ITEM = "Cart Item";
+    let PLACE_ORDER = "Place Order";
+    let PAYMENT_RECEIVED = "Payment Received";
     return (
         <div>
+            <h2>generate a report with the items sold each month</h2>
             <label htmlFor="month">Month:</label>
             <input
                 id="month"
@@ -36,27 +93,56 @@ export function AdminReport() {
                 value={year}
                 onChange={e => setYear(e.target.value)}
             />
-            <button onClick={getOrderReport}></button>
-        <table>
-            <thead>
-            <tr>
-                <th>Item ID</th>
-                <th>Item Name</th>
-                <th>Quantity</th>
-                <th>Price</th>
-            </tr>
-            </thead>
-            <tbody>
-            {data.map(item => (
-                <tr key={item.item_id}>
-                    <td>{item.item_id}</td>
-                    <td>{item.item_name}</td>
-                    <td>{item.quantity}</td>
-                    <td>{item.price}</td>
-                </tr>
-            ))}
-            </tbody>
-        </table>
+            <Button onClick={getOrderReport}>see report</Button>
+            <Table
+                columns={columnsO}
+                dataSource={data}
+                pagination={{pageSize: 10}}
+            />
+
+            <h2>generate a report with the items sold each month</h2>
+            <label htmlFor="fromMonth">From Month:</label>
+            <input
+                id="fromMonth"
+                type="number"
+                value={fromMonth}
+                onChange={e => setFromMonth(e.target.value)}
+            />
+            <label htmlFor="fromYear">From Year:</label>
+            <input
+                id="fromYear"
+                type="number"
+                value={fromYear}
+                onChange={e => setFromYear(e.target.value)}
+            />
+            <br/>
+            <label htmlFor="toMonth">To Month:</label>
+            <input
+                id="toMonth"
+                type="number"
+                value={toMonth}
+                onChange={e => setToMonth(e.target.value)}
+            />
+            <label htmlFor="toYear">To Year:</label>
+            <input
+                id="toYear"
+                type="number"
+                value={toYear}
+                onChange={e => setToYear(e.target.value)}
+            />
+            <label htmlFor="type">Type:</label>
+            <select id="type" value={type} onChange={e => setType(e.target.value)}>
+                <option value={VIEW_ITEM}>{VIEW_ITEM}</option>
+                <option value={CART_ITEM}>{CART_ITEM}</option>
+                <option value={PLACE_ORDER}>{PLACE_ORDER}</option>
+                <option value={PAYMENT_RECEIVED}>{PAYMENT_RECEIVED}</option>
+            </select>
+            <Button onClick={getUsageVisitEvents}>see VisitEvents</Button>
+            <Table
+                columns={columnsV}
+                dataSource={data2}
+                pagination={{pageSize: 10}}
+            />
         </div>
     );
 
--- src/MainPage.js
+++ src/MainPage.js
@@ -1,72 +1,75 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, {useState, useEffect} from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 
 import App from "./App";
 import ItemDetails from "./ItemDetails";
 import Login from "./Login";
 import Register from "./Register";
 import ShoppingCart from "./ShoppingCart";
-import { getAllCartItems, getCartMock } from "./api/shoppingCartApi";
-import { AdminReport } from "./AdminReport";
+import {getAllCartItems, getCartMock} from "./api/shoppingCartApi";
+import {AdminReport} from "./AdminReport";
 import Checkout from "./Checkout";
-import { ThemeProvider } from "@mui/material/styles";
+import {ThemeProvider} from "@mui/material/styles";
 import Header from "./components/Header";
+import RecoveryQuestion from "./RecoveryQuestion";
+import ResetPassword from "./Login";
 
 function MainPage() {
-  const [data, setData] = useState([]);
-  const [isLogin, setIslogin] = useState(localStorage.getItem("token"));
-  // const [isLogin, setIslogin] = useState(false)
-  const [isAdmin, setIsAdmin] = useState(
-    Boolean(eval(localStorage.getItem("is_admin")))
-  );
-  // Fetch the cart data on component mount
-  useEffect(() => {
-    getAllCartItems().then((res) => {
-      setData(res.data.data);
-    });
-  }, []);
+    const [data, setData] = useState([]);
+    const [isLogin, setIslogin] = useState(localStorage.getItem("token"));
+    const [isAdmin, setIsAdmin] = useState(
+        Boolean(eval(localStorage.getItem("is_admin")))
+    );
+    // Fetch the cart data on component mount
+    useEffect(() => {
+        getAllCartItems().then((res) => {
+            setData(res.data.data);
+        });
+    }, []);
 
-  return (
-    <Router>
-      <div>
-        <Header data={data} />
+    return (
+        <Router>
+            <div>
+                <Header data={data}/>
 
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <App
-                data={data}
-                setData={setData}
-                isLogin={isLogin}
-                isAdmin={isAdmin}
-              />
-            }
-          ></Route>
-          <Route
-            path="/details/:id"
-            element={
-              <ItemDetails data={data} setData={setData} isLogin={isLogin} />
-            }
-          ></Route>
-          <Route
-            path="/shoppingCart"
-            element={
-              <ShoppingCart data={data} setData={setData} isLogin={isLogin} />
-            }
-          ></Route>
-          <Route
-            path="/login"
-            element={<Login setIslogin={setIslogin} setIsAdmin={setIsAdmin} />}
-          ></Route>
-          <Route path="/register" element={<Register />}></Route>
-          <Route path="/adminReport" element={<AdminReport />}></Route>
-          <Route path="/checkout" element={<Checkout data={data} />}></Route>
-        </Routes>
-      </div>
-    </Router>
-  );
+                <Routes>
+                    <Route
+                        exact
+                        path="/"
+                        element={
+                            <App
+                                data={data}
+                                setData={setData}
+                                isLogin={isLogin}
+                                isAdmin={isAdmin}
+                            />
+                        }
+                    ></Route>
+                    <Route
+                        path="/details/:id"
+                        element={
+                            <ItemDetails data={data} setData={setData} isLogin={isLogin}/>
+                        }
+                    ></Route>
+                    <Route
+                        path="/shoppingCart"
+                        element={
+                            <ShoppingCart data={data} setData={setData} isLogin={isLogin}/>
+                        }
+                    ></Route>
+                    <Route
+                        path="/login"
+                        element={<Login setIslogin={setIslogin} setIsAdmin={setIsAdmin}/>}
+                    ></Route>
+                    <Route path="/register" element={<Register/>}></Route>
+                    <Route path="/adminReport" element={<AdminReport/>}></Route>
+                    <Route path="/checkout" element={<Checkout data={data}/>}></Route>
+                    <Route path="/RecoveryQuestion" element={<RecoveryQuestion/>}></Route>
+                    <Route path="/resetPassword" element={<ResetPassword/>}></Route>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default MainPage;
